f7abec8b36dbf8990229002779a4d351f9a68621
André Tashiro <andre_seichi@hotmail.com>
2021-08-20 18:35:36 -0300

    cards info

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -11,13 +11,25 @@ export const Cards = () => {
           <ul className="cards__items">
             <CardItem
               src="images/jett.png"
-              text="Jett adsad asdada dad d asdad asda sdas d asa sd"
+              text="Jett kawaii with your kunais and nice voice"
               label="Jett"
               path="/services"
             />
             <CardItem
+              src="images/raze.png"
+              text="Raze to make unnecessary jumps and then explode your allies"
+              label="Raze"
+              path="/services"
+            />
+            <CardItem
+              src="images/breach.png"
+              text="Breach to flash your allies and then use his ult to stun for your enemies"
+              label="Breach"
+              path="/services"
+            />
+            <CardItem
               src="images/sage.jpg"
-              text="Sage God > All"
+              text="Sage god to heal yourself only and wall the bombsite to prevent the team from escaping the spike detonation"
               label="Sage"
               path="/services"
             />
