063d3554a0c7636c18a62f871d3627933966a560
André Tashiro <andre_seichi@hotmail.com>
2021-08-20 18:07:16 -0300

    Cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import { Cards } from '../components/Cards';
 import { HeroSection } from '../components/HeroSection';
 
 export const Home = () => {
   return (
     <>
       <HeroSection />
+      <Cards />
     </>
   );
 };
