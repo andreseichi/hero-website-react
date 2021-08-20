960d06f4efcb5e8afbe1ca481e872b8712618e7d
André Tashiro <andre_seichi@hotmail.com>
2021-08-20 18:07:09 -0300

    Cards component

---
+++ src/components/Cards/index.jsx
@@ -0,0 +1,29 @@
+import './style.css';
+
+import { CardItem } from '../CardItem';
+
+export const Cards = () => {
+  return (
+    <div className="cards">
+      <h2>Check out these EPIC Destinations</h2>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <CardItem
+              src="images/jett.png"
+              text="Jett adsad asdada dad d asdad asda sdas d asa sd"
+              label="Jett"
+              path="/services"
+            />
+            <CardItem
+              src="images/sage.jpg"
+              text="Sage God > All"
+              label="Sage"
+              path="/services"
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
