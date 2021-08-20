f4ca50ce2305ebc0da76c9af623b377e9cf3031d
André Tashiro <andre_seichi@hotmail.com>
2021-08-20 11:48:53 -0300

    HeroSection component

---
+++ src/components/HeroSection/index.jsx
@@ -0,0 +1,42 @@
+import './style.css';
+
+import { Button } from '../Button';
+
+import backgroundVideo from '../../assets/videos/Retake-hero-desktop.mp4';
+import posterVideo from '../../assets/images/Retake-hero-poster.jpg';
+
+export const HeroSection = () => {
+  return (
+    <div className="hero-container">
+      <video
+        preload="true"
+        src={backgroundVideo}
+        autoPlay
+        loop
+        muted
+        poster={posterVideo}
+      />
+
+      <h1>GO VAVA</h1>
+      <p>Let's play valorant?</p>
+
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          GET STARTED
+        </Button>
+
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+        >
+          WATCH TRAILER <i far fa-play-circle />
+        </Button>
+      </div>
+    </div>
+  );
+};
